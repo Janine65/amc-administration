138ef171aa2ef06cbd4da99af76fc50cb9b68410
Janine Franken <janine@olconet.com>
2020-05-16 10:07:20 +0200

    stable version

--- controllers/grid.js
+++ controllers/grid.js
@@ -2,42 +2,46 @@ var db = require("../db");
 const { Op, Serialize } = require("sequelize");
 
 module.exports = {
-	getData : function(req, res){
-		db.Adressen.findAll({where: {austritt: {[Op.eq]: null}} }).then(data => res.json(data));
+	getData: function (req, res) {
+		db.Adressen.findAll({ where: { 
+			[Op.or]: [ 
+			{austritt: { [Op.eq]: null } },
+			{austritt: { [Op.gte]: new Date() }}]
+			 }}).then(data => res.json(data));
 	},
 
 
-	removeData: function(req, res){
+	removeData: function (req, res) {
 		const data = req.body;
 		db.Adressen.findByPk(req.params.id)
 			.then((data) =>
 				data.austritt = Serialize.NOW)
-			.then((adresse) => 
+			.then((adresse) =>
 				//adresse.destroy()
 				adresse.update(data))
-			.then(() => 
+			.then(() =>
 				res.json({}));
 	},
 
-	addData: function(req, res){
+	addData: function (req, res) {
 		const data = req.body;
 		// force null values
 		if (!data.anrede)
 			data.anrede = 1;
 
-		db.Adressen.create(req.body).then((obj) => 
-				res.json({ id: obj.id }));
+		db.Adressen.create(req.body).then((obj) =>
+			res.json({ id: obj.id }));
 	},
-	updateData: function(req, res){
+	updateData: function (req, res) {
 		const data = req.body;
 		// force null values
 		if (!data.anrede_id)
 			data.anrede_id = 1;
-		
+
 		db.Adressen.findByPk(req.params.id)
-			.then((adresse) => 
+			.then((adresse) =>
 				adresse.update(data))
-			.then(() => 
+			.then(() =>
 				res.json({}));
 	}
 };
